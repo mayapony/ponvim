b223c10203c40c89c239d1f5f872da1ac854ab22
mayapony <mayapony@foxmail.com>
2024-04-13 11:59:11 +0800

    ✨ 修改了一些插件

--- lua/plugins/coding/autopairs.lua
+++ lua/plugins/coding/autopairs.lua
@@ -1,16 +1,19 @@
 return {
-  event = "InsertEnter",
-  "windwp/nvim-autopairs",
-  priority = 10000,
-  config = function()
-    require("nvim-autopairs").setup({ enable_check_bracket_line = false })
-
-    -- setup cmp for autopairs
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
-  opts = {
-    fast_wrap = {},
-    disable_filetype = { "TelescopePrompt", "vim" },
-  },
+	"echasnovski/mini.pairs",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{
+			"<leader>tp",
+			function()
+				vim.g.minipairs_disable = not vim.g.minipairs_disable
+				if vim.g.minipairs_disable then
+					print("Disabled auto pairs")
+				else
+					print("Enabled auto pairs")
+				end
+			end,
+			desc = "Toggle Auto Pairs",
+		},
+	},
 }
