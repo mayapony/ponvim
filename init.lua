27f1b6109f93a80e7476101acb8117dc92ce5052
mayapony <mayapony@foxmail.com>
2024-06-02 22:13:36 +0800

    🐛 修改vscode配置的位置

--- init.lua
+++ init.lua
@@ -2,74 +2,76 @@
 -- source: https://github.com/folke/lazy.nvim#-installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 if not vim.g.vscode then
-  -- load nvim config
-  require("config.options")
-  require("config.autocmd")
-  require("config.global")
+	-- load nvim config
+	require("config.options")
+	require("config.autocmd")
+	require("config.global")
 
-  require("config.keymaps").initNvim()
+	require("config.keymaps").initNvim()
 
-  require("lazy").setup({
-    spec = {
-      { import = "plugins" },
-      { import = "plugins.lang" },
-      { import = "plugins.tools" },
-      { import = "plugins.ui" },
-      { import = "plugins.coding" },
-    },
-    checker = { enabled = false },
-    change_detection = {
-      -- automatically check for config file changes and reload the ui
-      enabled = true,
-      notify = false, -- get a notification when changes are found
-    },
-    defaults = {
-      version = false,
-      lazy = true,
-    },
-    performance = {
-      rtp = {
-        -- disable some rtp plugins
-        disabled_plugins = {
-          "netrw",
-          "netrwPlugin",
-          "netrwSettings",
-          "netrwFileHandlers",
-          "gzip",
-          -- "matchit",
-          -- "matchparen",
-          -- "netrwPlugin",
-          "tarPlugin",
-          "tohtml",
-          "tutor",
-          "zipPlugin",
-        },
-      },
-    },
-  })
+	require("lazy").setup({
+		spec = {
+			{ import = "plugins" },
+			{ import = "plugins.lang" },
+			{ import = "plugins.tools" },
+			{ import = "plugins.ui" },
+			{ import = "plugins.coding" },
+		},
+		checker = { enabled = false },
+		change_detection = {
+			-- automatically check for config file changes and reload the ui
+			enabled = true,
+			notify = false, -- get a notification when changes are found
+		},
+		defaults = {
+			version = false,
+			lazy = true,
+		},
+		performance = {
+			rtp = {
+				-- disable some rtp plugins
+				disabled_plugins = {
+					"netrw",
+					"netrwPlugin",
+					"netrwSettings",
+					"netrwFileHandlers",
+					"gzip",
+					-- "matchit",
+					-- "matchparen",
+					-- "netrwPlugin",
+					"tarPlugin",
+					"tohtml",
+					"tutor",
+					"zipPlugin",
+				},
+			},
+		},
+	})
 else
-  local vscode = require("vscode")
-  vscode.configure()
+	local vscodeConfig = require("config.vscode")
+	vscodeConfig.configure()
+	vscodeConfig.keymaps()
 
-  local options = {
-    root = vim.fn.stdpath("data") .. "/lazy-vscode",
-    lockfile = vim.fn.stdpath("config") .. "/lazy-vscode-lock.json",
-  }
-  if vim.loop.os_uname().version:match("Windows") then
-    options.concurrency = 1
-  end
+	local options = {
+		root = vim.fn.stdpath("data") .. "/lazy-vscode",
+		lockfile = vim.fn.stdpath("config") .. "/lazy-vscode-lock.json",
+	}
 
-  require("lazy").setup(vscode.packages(), options)
+	if vim.loop.os_uname().version:match("Windows") then
+		options.concurrency = 1
+	end
+
+	require("lazy").setup(vscodeConfig.packages(), options)
 end
