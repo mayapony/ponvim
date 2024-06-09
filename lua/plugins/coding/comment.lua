073ab9d13797e505d980fba3c8cedde9487df0d8
mayapony <mayapony@foxmail.com>
2024-06-09 17:20:19 +0800

    🐛 更新comment的配置

--- lua/plugins/coding/comment.lua
+++ lua/plugins/coding/comment.lua
@@ -1,20 +1,13 @@
 return {
-	{
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		lazy = true,
-		opts = {
-			enable_autocmd = false,
-		},
-	},
-	{
-		"echasnovski/mini.comment",
-		event = "VeryLazy",
-		opts = {
-			options = {
-				custom_commentstring = function()
-					return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-				end,
-			},
-		},
-	}
+  "echasnovski/mini.comment",
+  event = "VeryLazy",
+  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", opts = true },
+  opts = {
+    options = {
+      custom_commentstring = function()
+        vim.g.skip_ts_context_commentstring_module = true
+        return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+      end,
+    },
+  },
 }
