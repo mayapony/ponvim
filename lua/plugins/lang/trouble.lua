c9c5e2fbdcd33305f3126f07ebebe621cd09b6d1
mayapony <mayapony@foxmail.com>
2024-06-11 20:17:03 +0800

    🐛 修复 trouble 更新后无法使用

--- lua/plugins/lang/trouble.lua
+++ lua/plugins/lang/trouble.lua
@@ -1,64 +1,37 @@
 return {
-  "folke/trouble.nvim",
-  cmd = { "TroubleToggle", "Trouble" },
-  opts = { use_diagnostic_signs = true },
-  keys = {
-    {
-      "<leader>xx",
-      function()
-        require("trouble").open()
-      end,
-      desc = "Toggle Trouble",
-    },
-    {
-      "<leader>xw",
-      function()
-        require("trouble").open("workspace_diagnostics")
-      end,
-      desc = "Workspace Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xd",
-      function()
-        require("trouble").open("document_diagnostics")
-      end,
-      desc = "Document Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xq",
-      function()
-        require("trouble").open("quickfix")
-      end,
-      desc = "Quickfix List (Trouble)",
-    },
-    {
-      "<leader>xl",
-      function()
-        require("trouble").open("loclist")
-      end,
-      desc = "Location List (Trouble)",
-    },
-    {
-      "[q",
-      function()
-        if require("trouble").is_open() then
-          require("trouble").previous({ skip_groups = true, jump = true })
-        else
-          vim.cmd.cprev()
-        end
-      end,
-      desc = "Previous trouble/quickfix item",
-    },
-    {
-      "]q",
-      function()
-        if require("trouble").is_open() then
-          require("trouble").next({ skip_groups = true, jump = true })
-        else
-          vim.cmd.cnext()
-        end
-      end,
-      desc = "Next trouble/quickfix item",
-    },
-  },
+	"folke/trouble.nvim",
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=true win.position=bottom<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 }
--- lua/plugins/ui/indentline.lua
+++ lua/plugins/ui/indentline.lua
@@ -1,50 +1,51 @@
 return {
-  "shellRaining/hlchunk.nvim",
-  event = { "BufRead" },
-  config = function()
-    require("hlchunk").setup({
-      chunk = {
-        enable = true,
-        use_treesitter = true,
-        notify = true, -- notify if some situation(like disable chunk mod double time)
-        exclude_filetypes = {
-          aerial = true,
-          dashboard = true,
-          Trouble = true,
-          ["neo-tree"] = true,
-          ["neo-tree-popup"] = true,
-          [""] = true,
-        },
-        support_filetypes = {
-          "*.lua",
-          "*.js",
-          "*.jsx",
-          "*.ts",
-          "*.tsx",
-          "*.json",
-        },
-        chars = {
-          horizontal_line = "─",
-          vertical_line = "│",
-          left_top = "╭",
-          left_bottom = "╰",
-          right_arrow = ">",
-        },
-        style = {
-          { fg = vim.fn.synIDattr(vim.fn.hlID("IndentlineStyle"), "fg", "gui") },
-        },
-      },
+	"shellRaining/hlchunk.nvim",
+	event = { "BufRead" },
+	config = function()
+		require("hlchunk").setup({
+			chunk = {
+				enable = true,
+				use_treesitter = true,
+				notify = true, -- notify if some situation(like disable chunk mod double time)
+				exclude_filetypes = {
+					aerial = true,
+					dashboard = true,
+					Trouble = true,
+					["neo-tree"] = true,
+					["neo-tree-popup"] = true,
+					gitconfig = true,
+					[""] = true,
+				},
+				support_filetypes = {
+					"*.lua",
+					"*.js",
+					"*.jsx",
+					"*.ts",
+					"*.tsx",
+					"*.json",
+				},
+				chars = {
+					horizontal_line = "─",
+					vertical_line = "│",
+					left_top = "╭",
+					left_bottom = "╰",
+					right_arrow = ">",
+				},
+				style = {
+					{ fg = vim.fn.synIDattr(vim.fn.hlID("IndentlineStyle"), "fg", "gui") },
+				},
+			},
 
-      indent = {
-        enable = false,
-      },
+			indent = {
+				enable = false,
+			},
 
-      line_num = {
-        enable = false,
-      },
-      blank = {
-        enable = false,
-      },
-    })
-  end,
+			line_num = {
+				enable = false,
+			},
+			blank = {
+				enable = false,
+			},
+		})
+	end,
 }
