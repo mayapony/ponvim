0e834b9ae1eb485f0b81098e481cf1a461ff9084
mayapony <mayapony@foxmail.com>
2025-12-20 22:06:12 +0800

    ♻️ 优化 which-key 的显示

--- lua/plugins/tools/harpoon.lua
+++ lua/plugins/tools/harpoon.lua
@@ -1,65 +1,57 @@
-local function get_harpoon_keymaps()
-	local keymaps = {
-		{
-			"<leader>hm",
-			function()
-				require("harpoon"):list():add()
-			end,
-			desc = "[H]arpoon [M]ark",
-		},
-		{
-			"<leader>ho",
-			function()
-				require("harpoon").ui.toggle_quick_menu(require("harpoon"):list())
-			end,
-			desc = "[H]arpoon [O]pen quick menu",
-		},
-		{
-			"<leader>hp",
-			function()
-				require("harpoon"):list():prev()
-			end,
-			desc = "[H]arpoon [P]revious",
-		},
-		{
-			"<leader>hn",
-			function()
-				require("harpoon"):list():next()
-			end,
-			desc = "[H]arpoon [N]ext",
-		},
-	}
-
-	local quick_select_keys = "asdfghjkl"
-	for i = 1, #quick_select_keys do
-		local key = quick_select_keys:sub(i, i)
-		local keymap = {
-			"<leader>h" .. key,
-			function()
-				require("harpoon"):list():select(i)
-			end,
-			desc = "Harpoon: Select item " .. i,
-		}
-		table.insert(keymaps, keymap)
-	end
-
-	return keymaps
-end
-
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local harpoon = require("harpoon")
-
-		-- REQUIRED
 		harpoon:setup()
-		-- REQUIRED
+		local wk = require('which-key')
 
-		vim.keymap.set("n", "<C-e>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
+		local keymaps = {
+			{
+				"<leader>hm",
+				function()
+					require("harpoon"):list():add()
+				end,
+				desc = "[H]arpoon [M]ark",
+			},
+			{
+				"<leader>ho",
+				function()
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "[H]arpoon [O]pen quick menu",
+			},
+			{
+				"<leader>hp",
+				function()
+					require("harpoon"):list():prev()
+				end,
+				desc = "[H]arpoon [P]revious",
+			},
+			{
+				"<leader>hn",
+				function()
+					require("harpoon"):list():next()
+				end,
+				desc = "[H]arpoon [N]ext",
+			},
+		}
+
+		local quick_select_keys = "123456789"
+		for i = 1, #quick_select_keys do
+			local key = quick_select_keys:sub(i, i)
+			local keymap = {
+				"<leader>" .. key,
+				function()
+					require("harpoon"):list():select(i)
+				end,
+				desc = "Harpoon: Select item " .. i,
+				hidden = true
+			}
+			table.insert(keymaps, keymap)
+		end
+
+		wk.add(keymaps)
 	end,
-	keys = get_harpoon_keymaps(),
 }
